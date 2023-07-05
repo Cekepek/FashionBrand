3b39ac4bf3cd3b910465b7c17d7bf8e91b4620e4
badInventor101 <meteorboy3@gmail.com>
2023-07-05 14:46:21 +0700

    erja_

--- database/migrations/2023_07_05_065112_create_products_table.php
+++ database/migrations/2023_07_05_065112_create_products_table.php
@@ -17,7 +17,6 @@ class CreateProductsTable extends Migration
             $table->unsignedBigInteger('cat_id');
             $table->unsignedBigInteger('prod_type_id');
             $table->unsignedBigInteger('brand_id');
-            $table->unsignedBigInteger('dim_id');
             $table->foreign('cat_id')->references('id')->on('categories');
             $table->foreign('prod_type_id')->references('id')->on('product_types');
             $table->foreign('brand_id')->references('id')->on('brands');
