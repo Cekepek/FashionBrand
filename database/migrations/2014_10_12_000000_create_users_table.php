012d50b64a7b09462558bd927004bbacd9957f9c
badInventor101 <meteorboy3@gmail.com>
2023-07-05 15:42:25 +0700

    yes_

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,10 +14,13 @@ class CreateUsersTable extends Migration
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->string('username')->primary();
+            $table->id();
             $table->string('name');
+            $table->string('email')->unique();
+            $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->enum('role',['owner', 'staff', 'pembeli']);
+            $table->enum("role", ["owner", "pegawai"]);
+            $table->rememberToken();
             $table->timestamps();
         });
     }
