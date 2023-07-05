ebafc9c2aadcaa6ad4065082f266ead4ba1a0121
Cekepek <ckpk7171@gmail.com>
2023-07-05 13:34:55 +0700

    database migration

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,12 +14,10 @@ class CreateUsersTable extends Migration
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->id();
+            $table->string('username')->primary();
             $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->rememberToken();
+            $table->enum('role',['owner', 'staff', 'pembeli']);
             $table->timestamps();
         });
     }
