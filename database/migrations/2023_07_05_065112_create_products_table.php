fe6a3651351a69224257ca4203621d5b6917e097
Cekepek <ckpk7171@gmail.com>
2023-07-05 14:29:46 +0700

    products

--- database/migrations/2023_07_05_065112_create_products_table.php
+++ database/migrations/2023_07_05_065112_create_products_table.php
@@ -14,7 +14,17 @@ class CreateProductsTable extends Migration
     public function up()
     {
         Schema::create('products', function (Blueprint $table) {
+            $table->unsignedBigInteger('cat_id');
+            $table->unsignedBigInteger('prod_type_id');
+            $table->unsignedBigInteger('brand_id');
+            $table->unsignedBigInteger('dim_id');
+            $table->foreign('cat_id')->references('id')->on('categories');
+            $table->foreign('prod_type_id')->references('id')->on('product_types');
+            $table->foreign('brand_id')->references('id')->on('brands');
+            $table->foreign('dim_id')->references('id')->on('dimensions');
             $table->id();
+            $table->string('name', 200);
+            $table->integer('price', 200);
             $table->timestamps();
         });
     }
